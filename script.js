edad4a75201efc890e287b64e541f23078f0d6a1
mgastonportillo <mgastonportillo@gmail.com>
2022-11-06 16:43:44 -0300

    completar clase 4

--- script.js
+++ script.js
@@ -1,21 +1,56 @@
-const btn = document.querySelector("[data-form-btn]");
+(() => {
+	const btn = document.querySelector("[data-form-btn]");
 
-const createTask = (event) => {
-	event.preventDefault();
-	const input = document.querySelector("[data-form-input]");
-	const list = document.querySelector("[data-list]");
-	const task = document.createElement("li");
-	const value = input.value;
-	const content = `
-  <div>
-    <i class="far fa-check-square icon"></i>
-    <span class="task">${value}</span>
-  </div>
-  <i class="fas fa-trash-alt trashIcon icon"></i>`;
-	task.classList.add("card");
-	task.innerHTML = content;
-	list.appendChild(task);
-	input.value = "";
-};
+	const completeTask = () => {
+		const i = document.createElement("i");
+		i.classList.add("far", "fa-check-square", "icon");
+		i.addEventListener("click", completeTaskHandler);
+		return i;
+	};
 
-btn.addEventListener("click", createTask);
+	const completeTaskHandler = (event) => {
+		const element = event.target;
+		element.classList.toggle("fas");
+		element.classList.toggle("completeIcon");
+		element.classList.toggle("far");
+	};
+
+	const removeTask = () => {
+		const i = document.createElement("i");
+		i.classList.add("fas", "fa-trash-alt", "trashIcon", "icon");
+		i.addEventListener("click", removeTaskHandler);
+		return i;
+	};
+
+	const removeTaskHandler = (event) => {
+		const element = event.target;
+	};
+
+	const createTask = (event) => {
+		event.preventDefault();
+
+		const input = document.querySelector("[data-form-input]");
+		const value = input.value;
+		const list = document.querySelector("[data-list]");
+
+		const task = document.createElement("li");
+		task.classList.add("card");
+
+		const taskContent = document.createElement("div");
+
+		const taskTitle = document.createElement("span");
+		taskTitle.classList.add("task");
+		taskTitle.innerText = value;
+
+		taskContent.appendChild(completeTask());
+		taskContent.appendChild(taskTitle);
+
+		task.appendChild(taskContent);
+		task.appendChild(removeTask());
+		list.appendChild(task);
+
+		input.value = "";
+	};
+
+	btn.addEventListener("click", createTask);
+})();
