8fe77ba06ba02ae39508df196ea5fbaf2c452e17
mgastonportillo <mgastonportillo@gmail.com>
2022-11-05 22:22:53 -0300

    completar clase 3

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 const btn = document.querySelector("[data-form-btn]");
-const input = document.querySelector("[data-form-input]");
 
 const createTask = (event) => {
 	event.preventDefault();
-	console.log(input.value);
+	const input = document.querySelector("[data-form-input]");
+	const list = document.querySelector("[data-list]");
+	const task = document.createElement("li");
+	const value = input.value;
+	const content = `
+  <div>
+    <i class="far fa-check-square icon"></i>
+    <span class="task">${value}</span>
+  </div>
+  <i class="fas fa-trash-alt trashIcon icon"></i>`;
+	task.classList.add("card");
+	task.innerHTML = content;
+	list.appendChild(task);
+	input.value = "";
 };
 
 btn.addEventListener("click", createTask);
