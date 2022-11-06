2c3304ccb15f980da9b8db064e002fa3c2ce2820
mgastonportillo <mgastonportillo@gmail.com>
2022-11-05 21:56:41 -0300

    completar clase 2

---
+++ script.js
@@ -0,0 +1,9 @@
+const btn = document.querySelector("[data-form-btn]");
+const input = document.querySelector("[data-form-input]");
+
+const createTask = (event) => {
+	event.preventDefault();
+	console.log(input.value);
+};
+
+btn.addEventListener("click", createTask);
